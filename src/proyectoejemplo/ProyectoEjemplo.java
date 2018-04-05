fb1ef141f16e7cbfd95b24e164298b4f3cf16b96
Juan <Juan@DESKTOP-5NU27OI>
2018-04-04 21:30:30 -0500

    se borraron los comentarios se agrego una variable se mostro la variable

--- src/proyectoejemplo/ProyectoEjemplo.java
+++ src/proyectoejemplo/ProyectoEjemplo.java
@@ -1,21 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyectoejemplo;
 
-/**
- *
- * @author Juan
- */
 public class ProyectoEjemplo {
 
-    /**
-     * @param args the command line arguments
-     */
+  
     public static void main(String[] args) {
-        // TODO code application logic here
+        int numero;
+        numero = 1;
+        System.out.print("el numero es"+ 1);
     }
     
 }
